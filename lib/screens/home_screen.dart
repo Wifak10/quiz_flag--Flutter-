44b578a47a4b0e5c8d69bfc84f6720750bb306dd
wifak10 <wifakayekpola123@gmail.com>
2025-02-17 10:47:38 +0100

    Ajout de l'écran d'accueil avec un bouton pour commencer le jeu des pays

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Bienvenue au quiz des pays 😍'),
+          centerTitle: true,
+        ),
+        body: Center(child: ElevatedButton(
+          onPressed: () {
+            onPressed:
+            () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) =>game_screen()));
+            };
+          },
+          child: Text('Commencer le jeu',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+          ),
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(horizontal:40,vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            backgroundColor: Colors.yellow[700]
+          ),
+        )));
+        
+  }
+  
+  Widget game_screen() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jeu des pays'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Game Screen'),
+      ),
+    );
+  }
+}
