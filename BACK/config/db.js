26d55b9bf99b9a110a797bf4a2cd738c87f22b6b
wifak10 <wifakayekpola123@gmail.com>
2025-02-18 10:56:57 +0100

    Ajout de la connexion à la base de données MySQL avec gestion des erreurs

--- BACK/config/db.js
+++ BACK/config/db.js
@@ -1,2 +1,18 @@
 const mysql = require('mysql2');
 require('dotenv').config();
+
+const connection = mysql.createConnection({
+host: process.env.DB_HOST,
+user:process.env.DB_USER,
+user:process.env.DB_PASSWORD,
+user:process.env.DB_NAME
+
+});
+
+connection.connect((err)=>{
+    if(err){
+        console.error('Erreur de connetion à la base de données :', err);
+        return;
+}
+console.log('Connecté à la base de données Mysql');
+});
