bc4feb7dace1a0a70a77e3d5f00ff1c8068d39d2
wifak10 <wifakayekpola123@gmail.com>
2025-02-18 12:13:25 +0100

    Ajout de la route pour récupérer le leaderboard avec gestion des erreurs

--- BACK/routes/leaderboard.routes.js
+++ BACK/routes/leaderboard.routes.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const {getLeaderboard} = require('../models/score.model');
+const Router = express.Router();
+
+//Route pour récupérer le leaderboard
+Router.get('/leaderboard',(req, res)=>{
+    getLeaderboard((err,results)=>{
+        if (err) {
+            return res.status(500).send('Erreur lors de la récupération du leaderboard');    
+        }
+        res.status(200).json(results); //Cela renvoie les meilleurs scores
+    })
+})
+module.exports = router ;
